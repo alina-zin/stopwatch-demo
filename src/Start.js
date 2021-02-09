16f1492e1e92a8792eaed2cce252cc134203be61
Alina Zinchenko <c0zial00@students.oamk.fi>
2021-02-09 12:13:32 +0200

    app ready

---
+++ src/Start.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, useRef } from 'react';
+import Toggle from './Toggle';
+
+export default function Clock() {
+    const timerRef = useRef();
+    const [time, setTime] = useState(new Date())
+
+    useEffect(() => {
+        console.log('componentdidmount');
+        start();
+
+        return () => {
+            console.log('componentwillmount');
+            stop();
+        };
+    }, [])
+
+    function start() {
+        const id = setInterval(() => {
+            setTime(new Date());
+        },1000);
+        timerRef.current = id;
+        console.log(timerRef.current);
+    }
+
+    function stop() {
+        clearInterval(timerRef.current);
+    }
+
+    return (
+        <div style={{margin: '30px'}}>
+            {time.toLocaleTimeString()}
+            <Toggle start={start} stop={stop}/>
+        </div>
+    )
+}
